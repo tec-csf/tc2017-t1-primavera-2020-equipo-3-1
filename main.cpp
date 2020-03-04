21648869ce126fd249a5f64a29a9962907f1f19b
NotEmilioAG99 <44034926+NotEmilioAG99@users.noreply.github.com>
2020-03-04 17:15:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ int main()
     system("sudo apt-get update");
     system("sudo apt install python3.6");
     system("sudo apt-get install python3-pip");
+    system("pip install -U pip");
+    system("pip install -U sympy");
     system("python3 polinomiosimp.py 2*n*2+1+2*n2+n*2");
     myfile.open ("file.txt");
     if (myfile.is_open())
