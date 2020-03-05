625c989df1bcc31d25a48c88cef3b78c1831408c
NotEmilioAG99 <44034926+NotEmilioAG99@users.noreply.github.com>
2020-03-05 08:02:09 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
     ifstream myfile;
     string aux;
     string cadena = "python polinomiosimp.py";
+    system("python --version");
     system("python3 polinomiosimp.py 2*n*2+1+2*n2+n*2");
     myfile.open ("file.txt");
     if (myfile.is_open())
