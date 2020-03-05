1bf9fd0deb72ab4ec3a7e6ea18de1e7824676378
NotEmilioAG99 <44034926+NotEmilioAG99@users.noreply.github.com>
2020-03-05 09:22:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ int main()
     ifstream myfile;
     string aux;
     string cadena = "python polinomiosimp.py";
-    system("conda install -c anaconda scipy");
-    system("python");
-    system("polinomiosimp.py 2*n*2+1+2*n2+n*2");
+    system("conda create --name Balenciaga python=3 scipy");
+    system("source activate Balenciaga");
+    system("python polinomiosimp.py 2*n*2+1+2*n2+n*2");
     myfile.open ("file.txt");
     if (myfile.is_open())
     {
