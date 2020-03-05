4a7e596bbeafcad27d7107ad7240c30a1aa016d0
NotEmilioAG99 <44034926+NotEmilioAG99@users.noreply.github.com>
2020-03-05 09:16:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ int main()
     string aux;
     string cadena = "python polinomiosimp.py";
     system("conda install -c anaconda scipy");
-    system("conda run polinomiosimp.py 2*n*2+1+2*n2+n*2");
+    system("python");
+    system("polinomiosimp.py 2*n*2+1+2*n2+n*2");
     myfile.open ("file.txt");
     if (myfile.is_open())
     {
