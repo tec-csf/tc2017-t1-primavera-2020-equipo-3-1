fd2e7ec352b0e6b7e76a3b576ffd2c54846bb35e
NotEmilioAG99 <44034926+NotEmilioAG99@users.noreply.github.com>
2020-03-05 07:56:43 -0600

    Update polinomiosimp.py

--- polinomiosimp.py
+++ polinomiosimp.py
@@ -1,11 +1,2 @@
-from sympy import *
-import sys
-lista = sys.argv
-def polinomio (poli):
-    n = symbols('n')
-    return(expand(poli))
-value= str(polinomio (sys.argv[1]))
-f=open('file.txt','w')
-f.write(value)
-f.close()
-
+n=2
+print(n)
