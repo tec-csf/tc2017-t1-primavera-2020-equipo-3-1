f9fc00ed522a6397e1f5d6ebdb242b7923172891
NotEmilioAG99 <44034926+NotEmilioAG99@users.noreply.github.com>
2020-03-05 08:43:16 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,8 @@ int main()
     ifstream myfile;
     string aux;
     string cadena = "python polinomiosimp.py";
-    system("sudo apt-get install python3.7");
-    system("python --version");
-    system("sudo apt-get install python3-pip");
-    system("pip3 install python-scipy");
+    system("sudo apt-get install pip");
+    system("pip install python-scipy");
     system("python3 polinomiosimp.py 2*n*2+1+2*n2+n*2");
     myfile.open ("file.txt");
     if (myfile.is_open())
