b18e506b4d7c0d11ff86ee2c686ee54220b0e9d5
NotEmilioAG99 <44034926+NotEmilioAG99@users.noreply.github.com>
2020-03-06 11:21:11 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,20 +9,23 @@ int main()
     string line;
     ifstream myfile;
     string aux;
-    string cadena = "python polinomiosimp.py";
+    string cadena = "(2*n**2)*(n**2)";
+    ofstream outFile;
+    outFile.open("file.txt");
+    outFile<<cadena<<endl;
+    outFile.close();
     system("sudo apt-get update");
-    system("sudo apt-get install python3-pip3");
-    system("pip install --user sympy");
-    system("python polinomiosimp.py 2*n**2+2*n**2");
+    system("sudo apt-get install python3-pip");
+    system("pip3 install sympy");
+    system("python3 polinomiosimp.py");
     myfile.open ("file.txt");
     if (myfile.is_open())
     {
-        while ( getline(myfile,line) )
+        while (getline(myfile,line) )
         {
             aux=line;
         }
-        myfile.close();
     }
     myfile.close();
-    cout<<line<<endl;
+    cout<<aux<<endl;
 }
