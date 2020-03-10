3ace9a2712b630dc7d0cb56f249e126b6f422c84
EstebanManrique <42215173+EstebanManrique@users.noreply.github.com>
2020-03-10 17:40:38 -0600

    Update polinomiosimp.py

--- polinomiosimp.py
+++ polinomiosimp.py
@@ -1,2 +1,11 @@
-import os
-print (os.getcwd())
+from sympy import *
+g=open('file.txt','r')
+simpli=g.readline()
+g.close()
+def polinomio (poli):
+    n = symbols('n')
+    return(expand(poli))
+value=str(polinomio(simpli))
+f=open('file.txt','w')
+f.write(value)
+f.close()
