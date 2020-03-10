39ff04a8918488016f133d4a3ceb5ac8f9484010
EstebanManrique <42215173+EstebanManrique@users.noreply.github.com>
2020-03-10 12:17:12 -0600

    Update ZonaPruebas.cpp

--- ZonaPruebas.cpp
+++ ZonaPruebas.cpp
@@ -61,7 +61,7 @@ class Pruebas
         {
             system("sudo apt-get update");
             system("sudo apt-get install --user python3-pip");
-            system("pip3 install --user sympy");
+            system("pip3 install user sympy");
         }
 
         /**
